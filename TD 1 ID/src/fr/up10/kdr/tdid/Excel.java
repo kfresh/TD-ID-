c0bd5103f8518b9a267ae73cb51465bab9dff20a
SAID Abdoul Kader <kdr_star@hotmail.com>
2014-11-27 22:18:48 +0100

    Finalisation Excel

--- TD 1 ID/src/fr/up10/kdr/tdid/Excel.java
+++ TD 1 ID/src/fr/up10/kdr/tdid/Excel.java
@@ -7,61 +7,61 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-
+import java.util.HashMap;
 
 public class Excel {
-    private Connection conn;
-    
-    public Excel()
-    {
-        super();
-    }
-    
-    public void connexion() throws SQLException, ClassNotFoundException
-    {
-    	
+	private Connection conn;
 
-        try
-        {
-            Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
-        }
-        catch (ClassNotFoundException ex) 
-        {
-            System.err.println("Erreur de chargement du driver.");
-        }
-        
-        try 
-        {
+	public Excel() {
+		super();
+	}
 
-            this.conn = DriverManager.getConnection("jdbc:odbc:Excel Files","", "");
-        }
-        catch (SQLException ex) 
-        {
-            System.err.println("Excel Erreur de connexion à la base de données.");
-        }  
-    }
-    
-    public void deconnexion()
-    {
-        try
-        {
-            this.conn.close();      
-        }
-        catch (SQLException ex) 
-        {
-            System.err.println("Excel Erreur de deconnexion à la base de données.");
-        }
-    }
-    
-    public void nbEtuOrigFrance(){
-    	Statement requete = null; 
-    	ResultSet resultat = null;
-    	try {
-    		
-    		connexion();
+	public void connexion() throws SQLException, ClassNotFoundException {
+
+		try {
+			Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+		} catch (ClassNotFoundException ex) {
+			System.err.println("Erreur de chargement du driver.");
+		}
+
+		try {
+
+			this.conn = DriverManager.getConnection("jdbc:odbc:Excel Files",
+					"", "");
+		} catch (SQLException ex) {
+			System.err
+					.println("Excel Erreur de connexion à la base de données.");
+		}
+	}
+
+	public void deconnexion() {
+		try {
+			this.conn.close();
+		} catch (SQLException ex) {
+			System.err
+					.println("Excel Erreur de deconnexion à la base de données.");
+		}
+	}
+
+	public Integer nbEtuOrigFrance(Integer nb) {
+		Statement requete = null;
+		ResultSet resultat = null;
+		HashMap<Integer, String> hsEtu = new HashMap<Integer, String>();
+
+		try {
+
+			connexion();
 			requete = conn.createStatement();
-			resultat= requete.executeQuery("Select * from [2006$] UNION Select * from [2007$]");
-			
+			resultat = requete
+					.executeQuery("Select * from [2006$] UNION Select * from [2007$]");
+			while (resultat.next()) {
+				if (resultat.getString(4).contains("etudiant")
+						&& resultat.getString(5).contains("France")) {
+					hsEtu.put(Integer.valueOf(resultat.getInt(1)),
+							resultat.getString(2));
+				}
+
+			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			System.err.println("Excel Erreur de deconnexion au SQL");
@@ -69,19 +69,57 @@ public class Excel {
 			// TODO Auto-generated catch block
 			System.err.println("Excel Erreur de deconnexion au fichier");
 		}
-    	 try {
-			while(resultat.next())
-			{
-				
-				System.out.println(resultat.getString(2));
+		
+		nb = Integer.valueOf(nb.intValue() + hsEtu.size());
+		return nb;
+		
+	}
+	
+	public void nbCoursPType(HashMap<String, Integer> hs) {
+		Statement requete = null;
+		ResultSet resultat = null;
+		HashMap<Integer, String> hsCM = new HashMap<Integer, String>();
+		HashMap<Integer, String> hsTP = new HashMap<Integer, String>();
+		HashMap<Integer, String> hsTD = new HashMap<Integer, String>();
+
+
+
+
+		try {
+
+			connexion();
+			requete = conn.createStatement();
+			resultat = requete
+					.executeQuery("Select * from [2006$] UNION Select * from [2007$]");
+			while (resultat.next()) {
+				if (resultat.getString(10).contains("CM")) {
+					hsCM.put(Integer.valueOf(resultat.getInt(8)),
+							resultat.getString(9));
+				}
+				if (resultat.getString(10).contains("TP")){
+					hsTP.put(Integer.valueOf(resultat.getInt(8)),
+							resultat.getString(9));
+				}
+				if (resultat.getString(10).contains("TD")){
+					hsTD.put(Integer.valueOf(resultat.getInt(8)),
+							resultat.getString(9));
+				}
 
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.err.println("Excel Erreur de deconnexion au SQL");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.err.println("Excel Erreur de deconnexion au fichier");
 		}
-
-    	
-    	//return nb;
-    }
+		
+		Integer nbCM = Integer.valueOf(hsCM.size());
+		Integer nbTD = Integer.valueOf(hsTD.size());
+		Integer nbTP = Integer.valueOf(hsTP.size());
+		
+		hs.put("CM", nbCM);
+		hs.put("TD", nbTD);
+		hs.put("TP", nbTP);
+	}
 }
--- TD 1 ID/src/fr/up10/kdr/tdid/Mediateur.java
+++ TD 1 ID/src/fr/up10/kdr/tdid/Mediateur.java
@@ -18,11 +18,10 @@ public class Mediateur {
 	private Oracle monOracle;
 	private Excel monExcel;
 
-	//S1 = source excel
-	//S2 = source oracle
-	//S3 = source xml
-	
-	
+	// S1 = source excel
+	// S2 = source oracle
+	// S3 = source xml
+
 	public Mediateur() {
 		monXML = new XML();
 		monOracle = new Oracle();
@@ -40,23 +39,29 @@ public class Mediateur {
 
 		dictionnaire.get("Etudiant")
 				.put("ID-Etudiant", new ArrayList<String>());
+		dictionnaire.get("Etudiant").get("ID-Etudiant").add("S1");
 		dictionnaire.get("Etudiant").get("ID-Etudiant").add("S2");
 		dictionnaire.get("Etudiant").get("ID-Etudiant").add("S3");
 
 		dictionnaire.get("Etudiant").put("Nom", new ArrayList<String>());
+		dictionnaire.get("Etudiant").get("Nom").add("S1");
 		dictionnaire.get("Etudiant").get("Nom").add("S2");
 		dictionnaire.get("Etudiant").get("Nom").add("S3");
 
 		dictionnaire.get("Etudiant").put("Prenom", new ArrayList<String>());
+		dictionnaire.get("Etudiant").get("Prenom").add("S1");
 		dictionnaire.get("Etudiant").get("Prenom").add("S2");
 
 		dictionnaire.get("Etudiant").put("FormationPrecedente",
 				new ArrayList<String>());
+
+		dictionnaire.get("Etudiant").get("FormationPrecedente").add("S1");
 		dictionnaire.get("Etudiant").get("FormationPrecedente").add("S2");
 		dictionnaire.get("Etudiant").get("FormationPrecedente").add("S3");
 
 		dictionnaire.get("Etudiant").put("PaysFormationPrecedente",
 				new ArrayList<String>());
+		// dictionnaire.get("Etudiant").get("PaysFormationPrecedente").add("S1");
 		dictionnaire.get("Etudiant").get("PaysFormationPrecedente").add("S2");
 		dictionnaire.get("Etudiant").get("PaysFormationPrecedente").add("S3");
 
@@ -66,10 +71,12 @@ public class Mediateur {
 
 		dictionnaire.get("Etudiant").put("NiveauInsertion",
 				new ArrayList<String>());
+		dictionnaire.get("Etudiant").get("NiveauInsertion").add("S1");
 		dictionnaire.get("Etudiant").get("NiveauInsertion").add("S2");
 		dictionnaire.get("Etudiant").get("NiveauInsertion").add("S3");
-		
+
 		dictionnaire.get("Etudiant").put("Provenance", new ArrayList<String>());
+		dictionnaire.get("Etudiant").get("Provenance").add("S1");
 		dictionnaire.get("Etudiant").get("Provenance").add("S2");
 		dictionnaire.get("Etudiant").get("Provenance").add("S3");
 
@@ -81,14 +88,17 @@ public class Mediateur {
 
 		dictionnaire.get("Enseignant").put("ID-Enseignant",
 				new ArrayList<String>());
+		dictionnaire.get("Enseignant").get("ID-Enseignant").add("S1");
 		dictionnaire.get("Enseignant").get("ID-Enseignant").add("S2");
 		dictionnaire.get("Enseignant").get("ID-Enseignant").add("S3");
 
 		dictionnaire.get("Enseignant").put("Nom", new ArrayList<String>());
+		dictionnaire.get("Enseignant").get("Nom").add("S1");
 		dictionnaire.get("Enseignant").get("Nom").add("S2");
 		dictionnaire.get("Enseignant").get("Nom").add("S3");
 
 		dictionnaire.get("Enseignant").put("Prenom", new ArrayList<String>());
+		dictionnaire.get("Enseignant").get("Prenom").add("S1");
 		dictionnaire.get("Enseignant").get("Prenom").add("S2");
 		dictionnaire.get("Enseignant").get("Prenom").add("S3");
 
@@ -99,17 +109,21 @@ public class Mediateur {
 		// ***********************************************
 
 		dictionnaire.get("Cours").put("ID-Cours", new ArrayList<String>());
+		dictionnaire.get("Cours").get("ID-Cours").add("S1");
 		dictionnaire.get("Cours").get("ID-Cours").add("S2");
 		dictionnaire.get("Cours").get("ID-Cours").add("S3");
 
 		dictionnaire.get("Cours").put("Libele", new ArrayList<String>());
+		dictionnaire.get("Cours").get("Libele").add("S1");
 		dictionnaire.get("Cours").get("Libele").add("S2");
 
 		dictionnaire.get("Cours").put("Type", new ArrayList<String>());
+		dictionnaire.get("Cours").get("Type").add("S1");
 		dictionnaire.get("Cours").get("Type").add("S2");
 		dictionnaire.get("Cours").get("Type").add("S3");
 
 		dictionnaire.get("Cours").put("Niveau", new ArrayList<String>());
+		dictionnaire.get("Cours").get("Niveau").add("S1");
 		dictionnaire.get("Cours").get("Niveau").add("S2");
 		dictionnaire.get("Cours").get("Niveau").add("S3");
 
@@ -120,19 +134,24 @@ public class Mediateur {
 
 		dictionnaire.get("Inscription").put("ID-Etudiant",
 				new ArrayList<String>());
+		dictionnaire.get("Inscription").get("ID-Etudiant").add("S1");
 		dictionnaire.get("Inscription").get("ID-Etudiant").add("S2");
 		dictionnaire.get("Inscription").get("ID-Etudiant").add("S3");
 
 		dictionnaire.get("Inscription")
 				.put("ID-Cours", new ArrayList<String>());
+
+		dictionnaire.get("Inscription").get("ID-Cours").add("S1");
 		dictionnaire.get("Inscription").get("ID-Cours").add("S2");
 		dictionnaire.get("Inscription").get("ID-Cours").add("S3");
 
 		dictionnaire.get("Inscription").put("Annee", new ArrayList<String>());
+		dictionnaire.get("Inscription").get("Annee").add("S1");
 		dictionnaire.get("Inscription").get("Annee").add("S2");
 		dictionnaire.get("Inscription").get("Annee").add("S3");
 
 		dictionnaire.get("Inscription").put("Note", new ArrayList<String>());
+		dictionnaire.get("Inscription").get("Note").add("S1");
 		dictionnaire.get("Inscription").get("Note").add("S2");
 		dictionnaire.get("Inscription").get("Note").add("S3");
 
@@ -155,77 +174,84 @@ public class Mediateur {
 
 	}
 
-	public void NbHeureEnseignant() throws FileNotFoundException, SAXException, IOException, ParserConfigurationException{
-		HashMap nbHeureEnsHs = new HashMap <Integer,Integer> ();
+	public void NbHeureEnseignant() throws FileNotFoundException, SAXException,
+			IOException, ParserConfigurationException {
+		HashMap<Integer, Integer> nbHeureEnsHs = new HashMap<Integer, Integer>();
 
-		if (dictionnaire.get("Enseigne").get("Heures").contains("S2"))
-		{
-			//La source Oracle ne contient pas cette donnée pour le moment
-			
+		if (dictionnaire.get("Enseigne").get("Heures").contains("S1")) {
+			// La source Excel ne contient pas cette donnée pour le moment
+
 		}
-		if (dictionnaire.get("Enseigne").get("Heures").contains("S3"))
-		{
-		monXML.nbHeuresEnseignant("Univ_BD_3.xml", nbHeureEnsHs);
+
+		if (dictionnaire.get("Enseigne").get("Heures").contains("S2")) {
+			// La source Oracle ne contient pas cette donnée pour le moment
+
 		}
-		
+		if (dictionnaire.get("Enseigne").get("Heures").contains("S3")) {
+			monXML.nbHeuresEnseignant("Univ_BD_3.xml", nbHeureEnsHs);
+		}
+
 		nbHeureEnsHs.size();
 
-		
-		
-		
 		Iterator it = nbHeureEnsHs.entrySet().iterator();
-		while (it.hasNext()){
+		while (it.hasNext()) {
 			Map.Entry ent = (Map.Entry) it.next();
-			
-			System.out.println("L'enseignant n°"+ ent.getKey().toString() +" a "+ ent.getValue() +" heures");
-			
+
+			System.out.println("L'enseignant n°" + ent.getKey().toString()
+					+ " a " + ent.getValue() + " heures");
+
 		}
-		
+
 	}
-	
-	public void nbEtuOrigFrance() throws FileNotFoundException, SAXException, IOException, ParserConfigurationException{
-		Integer nb = 0;
-		
-		if (dictionnaire.get("Etudiant").get("Provenance").contains("S3"))
-		{
-		nb = monXML.nbEtuOrigFrance("Univ_BD_3.xml", nb);
+
+	public void nbEtuOrigFrance() throws FileNotFoundException, SAXException,
+			IOException, ParserConfigurationException {
+		Integer nb = null;
+
+		if (dictionnaire.get("Etudiant").get("Provenance").contains("S1")) {
+			nb = monExcel.nbEtuOrigFrance(nb);
+
 		}
-		
-		if (dictionnaire.get("Etudiant").get("Provenance").contains("S2"))
-		{
-		nb = monOracle.nbEtuOrigFrance(nb);
-		
+
+		if (dictionnaire.get("Etudiant").get("Provenance").contains("S2")) {
+			nb = monOracle.nbEtuOrigFrance(nb);
+
 		}
-		System.out.println("il y a " + nb.toString() + " étudiants provenant de France");
-		
-		
+
+		if (dictionnaire.get("Etudiant").get("Provenance").contains("S3")) {
+			nb = monXML.nbEtuOrigFrance("Univ_BD_3.xml", nb);
+		}
+
+		System.out.println("il y a " + nb.toString()
+				+ " étudiants provenant de France");
+
 	}
-	
-	public void nbCourspType () throws FileNotFoundException, SAXException, IOException, ParserConfigurationException{
-		HashMap<String, Integer> hs =new HashMap<String, Integer>();
-		
+
+	public void nbCourspType() throws FileNotFoundException, SAXException,
+			IOException, ParserConfigurationException {
+		HashMap<String, Integer> hs = new HashMap<String, Integer>();
+
 		hs.put("CM", new Integer(0));
 		hs.put("TD", new Integer(0));
 		hs.put("TP", new Integer(0));
-		
-		
-		if (dictionnaire.get("Cours").get("Type").contains("S2"))
-		{
-		monOracle.nbCoursPType("Univ_BD_3.xml", hs);
+
+		if (dictionnaire.get("Cours").get("Type").contains("S1")) {
+			monExcel.nbCoursPType(hs);
 
 		}
-		if (dictionnaire.get("Cours").get("Type").contains("S3"))
-		{
-		monXML.nbCoursPType("Univ_BD_3.xml", hs);
+		if (dictionnaire.get("Cours").get("Type").contains("S2")) {
+			monOracle.nbCoursPType("Univ_BD_3.xml", hs);
+
+		}
+		if (dictionnaire.get("Cours").get("Type").contains("S3")) {
+			monXML.nbCoursPType("Univ_BD_3.xml", hs);
 
 		}
 
-		
-		System.out.println(hs.get("CM").toString()+ " Cours Magistaux");
-		System.out.println(hs.get("TD").toString()+ " Travaux dirigés");
-		System.out.println(hs.get("TP").toString()+ " Travaux pratiques");		
-		
-		
+		System.out.println(hs.get("CM").toString() + " Cours Magistaux");
+		System.out.println(hs.get("TD").toString() + " Travaux dirigés");
+		System.out.println(hs.get("TP").toString() + " Travaux pratiques");
+
 	}
 
 }
